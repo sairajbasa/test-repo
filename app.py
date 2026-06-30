fb289e35a5bf486949d4d640bcd01f3c668123bb
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-06-30 19:45:22 +0530

    app.py is created

---
+++ app.py
@@ -0,0 +1,10 @@
+# A very simple program
+
+# Imagine we are baking a cake
+cake_ready = False   # At first, the cake is not ready
+
+# We check if the cake is baked
+if cake_ready:
+    print("Yay! The cake is ready. Let's eat!")
+else:
+    print("Oops! The cake is not ready yet. Wait a little longer.")
