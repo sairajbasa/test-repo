31460fca7b9c64e06a3eb6c02176875524e6f91a
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-06-30 20:16:10 +0530

    new app.py code updated

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 # cake_check.py
 
 import time
-cake_ready = False   # At first, the cake is not ready
+import random
+
+cake_ready = False
 minutes_waited = 0
+
+# Random baking time between 3 and 7 minutes
+baking_time = random.randint(3, 7)
+
+print(f"The cake will take about {baking_time} minutes to bake...")
 
 while not cake_ready:
     print(f"Waiting... {minutes_waited} minutes passed.")
     time.sleep(1)   # pretend 1 second = 1 minute
     minutes_waited += 1
 
-    if minutes_waited >= 5:   # after 5 minutes, cake is ready
+    if minutes_waited >= baking_time:
         cake_ready = True
 
 if cake_ready:
     print("Yay! The cake is ready. Let's eat!")
+
 
